c8968556022294c5d793556ce1f8bb8fbc7b8658
Joseph <DeGregorio.Joseph@gmail.com>
2020-08-27 16:14:25 -0400

    working on user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ class User < ApplicationRecord
     has_secure_password
     validates :name, presence: true
     validates :email, presence: true
-    validates :email, confirmation: true
+    # validates :email, confirmation: true
     validates :email, uniqueness: true
-    validates :email_confirmation, presence: true
+    # validates :email_confirmation, presence: true
     validates :password, presence: true
-    validates :password, confirmation: true
-    validates :password_confirmation, presence: true
+    # validates :password, confirmation: true
+    # validates :password_confirmation, presence: true
 
 end
