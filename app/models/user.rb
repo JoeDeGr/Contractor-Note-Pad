788a15d27144d074c06f008805f5e6b5cbf79512
Joseph <DeGregorio.Joseph@gmail.com>
2020-08-27 15:40:12 -0400

    building User class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
     has_secure_password
     validates :name, presence: true
-    validates : 
+    validates :email, presence: true
+    validates :email, confirmation: true
+    validates :email, uniqueness: true
+    validates :email_confirmation, presence: true
+    validates :password, presence: true
+    validates :password, confirmation: true
+    validates :password_confirmation, presence: true
+
 end
