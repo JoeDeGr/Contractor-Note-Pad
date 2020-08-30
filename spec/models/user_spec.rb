ad36cead5f0b778a1558c7ba6d0431246c4ae339
Joseph <DeGregorio.Joseph@gmail.com>
2020-08-30 11:15:33 -0400

    hashing out rspec configuration.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
   it "is a User" do
-    user = User.create(name: "New User")
-    expect(user.name).to eq("New User")
+    user = User.create(name: "Johny Gongetsum", email: "Johnny@Johnny.com", password: "Johnny")
+    expect(user.name).to eq("Johny Gongetsum")
+    expect(user.email).to eq("Johnny@Johnny.com")
+    expect(user).to be_vaid
   end
 end
