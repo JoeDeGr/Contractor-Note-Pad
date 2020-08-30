29d18efe3f13652bb9f7db07f39638870cab4764
Joseph <DeGregorio.Joseph@gmail.com>
2020-08-30 11:21:06 -0400

    rspec is functioning

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
   it "is a User" do
-    user = User.create(name: "Johny Gongetsum", email: "Johnny@Johnny.com", password: "Johnny")
+    user = User.new(name: "Johny Gongetsum", email: "Johnny@Johnny.com", password: "Johnny")
+    expect(user.valid?).to eq(true)
     expect(user.name).to eq("Johny Gongetsum")
     expect(user.email).to eq("Johnny@Johnny.com")
-    expect(user).to be_vaid
+    
   end
 end
