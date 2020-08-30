845e5aabb79c0e8415988a12d033cf0a9ba6b1f9
Joseph <DeGregorio.Joseph@gmail.com>
2020-08-30 13:39:29 -0400

    building out user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,6 +16,12 @@ RSpec.describe User, type: :model do
   end
 
   it "is a User who can signup" do
+    visit"/users/new"
+    fill_in("name", :with => "Dirk Diggity")
+    fill_in("email", :with => "diggity_doggy@dirk.com")
+    fillin_in("password", :with => "password")
+    click_button('Create User')
+    expect(current_path).to be(user_path(user))
   end
 
 end
