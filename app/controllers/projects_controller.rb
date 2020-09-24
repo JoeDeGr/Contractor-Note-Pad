2b8ce17ff3b10a04eb3656760388a34b6152d3ea
Joseph <DeGregorio.Joseph@gmail.com>
2020-09-24 19:32:29 -0400

    created projects/update

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -18,6 +18,17 @@ class ProjectsController < ApplicationController
         @project = Project.find(params[:id])
     end
 
+    def edit
+        @project = Project.find(params[:id])
+        @user = @project.user
+    end
+
+    def update
+        @project = Project.find(params[:id])
+        @project.update(project_params)
+        redirect_to project_path(@project)
+    end
+
     private
 
     def project_params
