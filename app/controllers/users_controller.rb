755419d0fe483de2b1cb849b73342d7bb5fb9d7a
Joseph <DeGregorio.Joseph@gmail.com>
2020-09-27 13:27:00 -0400

    working out the user edit functionality, started working on worker/delete.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,10 +18,24 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
     end
 
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        @user.update(user_params)
+        if @user.valid? && (params[:password] == params[:password_confirmation])
+            redirect_to user_path(@user)
+        else
+            render 'edit'
+        end
+    end 
+
     private
 
     def user_params
-        params.require(:user).permit(:name, :email, :password)
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,9 @@ class User < ApplicationRecord
     has_secure_password
     validates :name, presence: true
     validates :email, presence: true
-    # validates :email, confirmation: true
     validates :email, uniqueness: true
-    # validates :email_confirmation, presence: true
     validates :password, presence: true
-    validates :password, confirmation: true
+    validates :password, confirmation: { case_sensitive: true }
     # validates :password_confirmation, presence: true
 
     has_many :projects
